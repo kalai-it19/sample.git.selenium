d7d3a5d281954fa3a4a324fbce6e93c491759757
kalaiselvi L <kalai.it19@gmail.com>
2018-12-20 15:48:41 -0800

    Created a file for gmail website

---
+++ MavenProject/src/test/java/selenium/GMailTest.java
@@ -0,0 +1,28 @@
+package selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class GMailTest {
+
+	@Test
+	public void gmail() {
+		try {
+			System.out.println("Called gmail!!!");
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe");
+
+			System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\Saba\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			ChromeDriver driver = new ChromeDriver(options);
+			
+			driver.get("https://www.google.com");
+			driver.switchTo().defaultContent();
+			driver.manage().window().maximize();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
