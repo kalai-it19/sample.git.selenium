a29fbf9049d53711913d9b4fdfbaf0fba95ec72d
kalaiselvi L <kalai.it19@gmail.com>
2018-12-20 17:00:49 -0800

    Added t mobile function

--- MavenProject/src/test/java/selenium/GMailTest.java
+++ MavenProject/src/test/java/selenium/GMailTest.java
@@ -1,30 +1,45 @@
 package selenium;
 
+import java.sql.Driver;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GMailTest {
-
+WebDriver driver = null;
+	@Before	
+	public void setup()
+	{
+		if (driver!= null)
+		{
+		System.out.println("Called gmail!!!");
+		ChromeOptions options = new ChromeOptions();
+		options.setBinary("C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Saba\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver(options);
+		}
+	}
+	
 	@Test
 	public void gmail() {
 		try {
-			System.out.println("Called gmail!!!");
-			ChromeOptions options = new ChromeOptions();
-			options.setBinary("C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe");
-
-			System.setProperty("webdriver.chrome.driver",
-					"C:\\Users\\Saba\\Downloads\\chromedriver_win32\\chromedriver.exe");
-			ChromeDriver driver = new ChromeDriver(options);
-			
 			driver.get("https://www.google.com");
 			driver.manage().window().maximize();
 			driver.navigate();
 			driver.quit();
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+@Test
+	public void tmobile() {
+driver.get("www.tmobile.com");
+	}
+
 }
