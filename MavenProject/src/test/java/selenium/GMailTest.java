287e8d5b0ff1e69ffb87d876b2c35ec605b1730e
kalaiselvi L <kalai.it19@gmail.com>
2018-12-20 17:56:41 -0800

    Code changes for tmobile

--- MavenProject/src/test/java/selenium/GMailTest.java
+++ MavenProject/src/test/java/selenium/GMailTest.java
@@ -9,36 +9,42 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GMailTest {
-WebDriver driver = null;
-	@Before	
-	public void setup()
-	{
-		if (driver!= null)
-		{
-		System.out.println("Called gmail!!!");
-		ChromeOptions options = new ChromeOptions();
-		options.setBinary("C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe");
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Saba\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver(options);
+	WebDriver driver = null;
+
+	@Before
+	public void setup() {
+		if (driver == null) {
+
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Program Files (x86)\\Google\\Chrome Beta\\Application\\chrome.exe");
+			System.setProperty("webdriver.chrome.driver",
+					"C:\\Users\\Saba\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			 driver = new ChromeDriver(options);
 		}
 	}
-	
+
 	@Test
 	public void gmail() {
 		try {
+			System.out.println("Called google!!!");
 			driver.get("https://www.google.com");
 			driver.manage().window().maximize();
-			driver.quit();
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-@Test
+
+	@Test
 	public void tmobile() {
-driver.get("www.tmobile.com");
+		try {
+			System.out.println("Called tmobile!!!");
+			driver.get("https://www.tmobile.com");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
